fd33f91ddaa9dc72bc8d14fb6b247e21c8e5a46c
oritsegbubemi <86294092+gbubemi22@users.noreply.github.com>
2022-05-13 16:59:53 +0100

    --- Auto-Git Commit ---

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 //import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
